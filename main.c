8caee2b28c9122ddf1facc9cce3b4c7a844a71a9
Ian Shi <ianshi1026@gmail.com>
2024-12-02 20:40:48 -0500

    forgot to commit from class

--- main.c
+++ main.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <linux/limits.h>
+
 int main() {
-	
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	printf("%s", cwd);
+
+	char input[200];
+	while(1) {
+		read(STDIN_FILENO, input, 200);
+		if (strlen(input) > 0) {
+			printf("%s", input);
+		}
+	}
 }
