24684980c93f90f5643d0fa95ee05b46b46c5180
Ian Shi <Ianshi1026@gmail.com>
2024-12-03 17:47:39 -0500

    put main in a loop

--- main.c
+++ main.c
@@ -15,20 +15,22 @@ int main() {
 		args[i] = malloc(sizeof(char) * 200);
 	}
 
-	prompt();
-	if(fgets(line, sizeof(line), stdin) != NULL) {
-		// Remove trailing newline if present
-        int len = strlen(line);
-        if (len > 0 && line[len - 1] == '\n') {
-            line[len - 1] = '\0';
-        }
-		
-		parse_args( line, args );
-	}
-	else {
-		perror("error reading user input");
-		exit(1);
-	}
+	while(1) {
+		prompt();
+		if(fgets(line, sizeof(line), stdin) != NULL) {
+			// Remove trailing newline if present
+			int len = strlen(line);
+			if (len > 0 && line[len - 1] == '\n') {
+				line[len - 1] = '\0';
+			}
+			
+			parse_args( line, args );
+		}
+		else {
+			perror("error reading user input");
+			exit(1);
+		}
 
-	execute_args(args);
+		execute_args(args);
+	}
 }
