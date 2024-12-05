5a548e5a93d433ae0f3c254f61db78dd248829db
echen500 <echen50@stuy.edu>
2024-12-05 03:38:54 -0500

    working on pipe

--- general.c
+++ general.c
@@ -5,6 +5,9 @@
 #include <sys/wait.h>
 #include "general.h"
 #include <linux/limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 void parse_commands(char * line, char ** com_ary) {
   char * curr = line;
   char * token;
@@ -44,6 +47,43 @@ void parse_args(char * line, char ** arg_ary) {
 // 		printf("\n");
 // 	}
 // }
+void execute_pipe(char * left, char * right) {
+  char *args[] = {"touch", "temp", "\0"};
+  execvp(args[0], args);
+ 
+  pid_t pid1 = fork();
+  if (pid1 == -1) {
+    perror("pipe fork");
+  }
+  if (pid1 == 0) {
+    int temp = open("temp", O_WRONLY);
+    int stdout = STDOUT_FILENO;
+    int backup_stdout = dup(stdout);
+    dup2(temp, stdout);
+    char *args[256];
+    parse_args(left, args);
+    execvp(args[0], args);
+    dup2(backup_stdout, stdout);
+    close(temp);
+    exit(1);
+  }
+  pid_t pid2 = fork();
+  if (pid2 == -1) {
+    perror("pipe fork 2");
+  }
+  if (pid2 == 0) {
+     int temp = open("temp", O_RDONLY);
+     int stdin = STDIN_FILENO;
+     int backup_stdin = dup(stdin);
+     dup2(stdin, temp);
+     char *args[256];
+    parse_args(right, args);
+    execvp(args[0], args);
+    dup2(backup_stdin, stdin);
+    close(temp);
+    exit(1);
+  }
+}
 
 void execute_commands(char ** commands) {
   int command_num = 0;
@@ -57,6 +97,15 @@ void execute_commands(char ** commands) {
         chdir(args[1]);
         command_num++;
         continue;
+    }
+    char *pipe = strchr(commands[command_num], '|');
+    if (pipe != NULL) {
+      char * curr = commands[command_num];
+      char * right = strsep(&curr, "|");
+      printf("%s", curr);
+      execute_pipe(curr, right);
+      command_num++;
+      continue;
     }
     pid_t pid = fork();
   	if (pid == -1) {
