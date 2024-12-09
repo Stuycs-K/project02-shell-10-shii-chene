0c2fee2434aac1e142573c93a478f4da30d876a2
echen500 <echen50@stuy.edu>
2024-12-09 03:13:51 -0500

    fixed cntl d

--- main.c
+++ main.c
@@ -9,23 +9,22 @@
 #include "utils.h"
 
 int main() {
-	char line[200];
-	char * commands[200];
-	while (1) {
-		prompt();
-		if(fgets(line, sizeof(line), stdin) != NULL) {
-			// Remove trailing newline if present
-	        int len = strlen(line);
-	        if (len > 0 && line[len - 1] == '\n') {
-	            line[len - 1] = '\0';
-	        }
-
-			parse_commands(line, commands);
-		}
-		else {
+    char line[200];
+    char * commands[200];
+    while (1) {
+        prompt();
+        if (fgets(line, sizeof(line), stdin) == NULL) {
+			printf("\n");
 			fflush(stdin);
-			exit(1);
-		}
-		execute_commands(commands);
-	}
+            exit(0);
+        }
+
+        int len = strlen(line);
+        if (len > 0 && line[len - 1] == '\n') {
+            line[len - 1] = '\0';
+        }
+
+        parse_commands(line, commands);
+        execute_commands(commands);
+    }
 }
